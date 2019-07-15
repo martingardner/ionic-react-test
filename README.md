09ad4dd5d83b4aebde8dbd8b257d16474b221e78
martin gardner II <martingardnerii@gmail.com>
2019-07-15 12:40:34 -0500

    update README and add in additional ui components to components page

--- README.md
+++ README.md
@@ -1,9 +1,19 @@
 # ionic-react-test
 testing ionic 4 + react
 
+#setup
+- clone repo
+- (if needed) npm install -g ionic@latest
+- npm install
+- ionic serve
+
 #instructions
 - instructions from https://ionicframework.com/docs/react/your-first-app
 
 # notes
 - you need to make sure your ionic version is 5+ or type=react won't be recognized
 - make sure your node and npm version is relatively up to date.  I initially ran into an issue with one of the dependencies not being found but updating my npm version and reinstalling everything fixed the issue.
+
+#useful pages
+- https://ionicframework.com/docs/react/your-first-app
+- https://ionicframework.com/docs/api
--- testReactIonic/src/pages/Components.tsx
+++ testReactIonic/src/pages/Components.tsx
@@ -1,7 +1,25 @@
-import {IonHeader, IonToolbar, IonTitle, IonContent} from '@ionic/react';
+import {
+    IonHeader, 
+    IonToolbar, 
+    IonTitle, 
+    IonContent, 
+    IonItem, 
+    IonLabel, 
+    IonDatetime,
+    IonList,
+    IonInput,
+    IonToggle,
+    IonRadioGroup,
+    IonRadio
+} from '@ionic/react';
 import React from 'react';
 
 const Components: React.FunctionComponent = () => {
+
+    const d = new Date();
+    let currentDate = `${ d.getFullYear() }-${ d.getMonth()+1 }-${ d.getDate() }`;
+
+   
     return (
         <>
             <IonHeader>
@@ -10,7 +28,43 @@ const Components: React.FunctionComponent = () => {
                 </IonToolbar>
             </IonHeader>
             <IonContent className="ion-padding">
-                jkjkjkj
+                <p>IonDatetime Component tests</p>
+                <IonItem>
+                    <IonLabel>MMMM</IonLabel>
+                    <IonDatetime displayFormat="MMMM" value={currentDate}>
+                    </IonDatetime>
+                </IonItem>
+                <IonItem>
+                    <IonLabel>MM DD YY</IonLabel>
+                    <IonDatetime displayFormat="MM DD YY" value={currentDate}>
+                    </IonDatetime>
+                </IonItem>
+            </IonContent>
+            <IonContent className="ion-padding">
+                <p>IonList Component tests</p>
+                <IonList>
+                    <IonItem>
+                        <IonLabel>Test Label</IonLabel>
+                    </IonItem>
+                    <IonItem>
+                        <IonLabel>Input</IonLabel>
+                        <IonInput></IonInput>
+                    </IonItem>
+                    <IonItem>
+                        <IonLabel>Toggle</IonLabel>
+                        <IonToggle ></IonToggle>
+                    </IonItem>
+                    <IonRadioGroup>
+                        <IonItem>
+                            <IonLabel>Option 1</IonLabel>
+                            <IonRadio value="1"></IonRadio>
+                        </IonItem>
+                        <IonItem>
+                            <IonLabel>Option 2</IonLabel>
+                            <IonRadio value="2"></IonRadio>
+                        </IonItem>
+                    </IonRadioGroup>
+                </IonList>
             </IonContent>
         </>
     )
